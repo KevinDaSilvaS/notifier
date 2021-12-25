e1521f19045fd38f614cede15d4b6a23dedad65f
Your Name <kevin.kds80@gmail.com>
2021-12-25 20:18:41 +0000

    add http codes and internal server error code

--- lib/notifier_web/http/response.ex
+++ lib/notifier_web/http/response.ex
@@ -1,5 +1,8 @@
 defmodule NotifierWeb.Response do
-  def response({:error, err}), do: %{ code: 400, details: err }
-  def response({:ok, data}), do: %{ code: 200, details: data }
-  def response({:created, data}), do: %{ code: 201, details: data }
+  import NotifierWeb.Codes
+
+  def response({:error, err}), do: %{ code: bad_request(), details: err }
+  def response({:ok, data}), do: %{ code: ok(), details: data }
+  def response({:created, data}), do: %{ code: bad_request(), details: data }
+  def response({_, data}), do: %{ code: internal_server_error(), details: data }
 end
