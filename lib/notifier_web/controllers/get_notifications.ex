42f8509b60a939e32d2463343790b08ac38d084c
Your Name <kevin.kds80@gmail.com>
2021-12-26 16:24:58 +0000

    fix limit and page extraction from map

--- lib/notifier_web/controllers/get_notifications.ex
+++ lib/notifier_web/controllers/get_notifications.ex
@@ -3,11 +3,11 @@ defmodule NotifierWeb.GetNotificationsController do
   import NotifierWeb.CouchDb.Operations, only: [get_notifications_operation: 3]
   import NotifierWeb.Response
 
-  @limit 15
-  @page 1
+  @limit "15"
+  @page "1"
   def get_notifications(conn, opts) do
-    limit = String.to_integer(opts["limit"]) || @limit
-    page  = String.to_integer(opts["page"])  || @page
+    limit = (opts["limit"] || @limit) |> String.to_integer()
+    page  = (opts["page"]  || @page)  |> String.to_integer()
 
     res = get_notifications_operation(opts["user"], limit, page) |> response()
     conn |> put_status(res.code) |> json(res)
