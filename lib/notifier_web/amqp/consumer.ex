1c57350795e6e2b7415f2d66038c419beafb012e
Your Name <kevin.kds80@gmail.com>
2022-01-22 22:58:29 +0000

    remove IO.inspect / IO.puts and add Logger instead

--- lib/notifier_web/amqp/consumer.ex
+++ lib/notifier_web/amqp/consumer.ex
@@ -1,6 +1,7 @@
 defmodule Consumer do
   use GenServer
   use AMQP, restart: :permanent
+  require Logger
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, [])
@@ -23,12 +24,12 @@ defmodule Consumer do
   end
 
   # Confirmation sent by the broker after registering this process as a consumer
-  def handle_info({:basic_consume_ok, %{consumer_tag: consumer_tag}}, chan) do
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, chan) do
     {:noreply, chan}
   end
 
   # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
-  def handle_info({:basic_cancel, %{consumer_tag: consumer_tag}}, chan) do
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, chan) do
     {:stop, :normal, chan}
   end
 
@@ -65,14 +66,14 @@ defmodule Consumer do
     socket = Agent.SocketState.get_socket_state()
     case socket do
       {:ok, socket} ->
-        IO.inspect payload
+        Logger.info("Message received: #{inspect(payload)}")
         {:ok, parsed_payload} = Jason.decode payload
         topic = "room:" <> parsed_payload["user_id"]
 
         {:ok, joined} = SocketClient.join_topic socket, topic
         SocketClient.push joined, topic, parsed_payload
         :ok = Basic.ack channel, tag
-      _ -> IO.inspect "ERROR"
+      _ -> Logger.error("Notifier is not up yet")
     end
   end
 end
