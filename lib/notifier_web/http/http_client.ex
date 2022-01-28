e9a935fbfb7f8efb16cb0857bfe1df238889960f
Your Name <kevin.kds80@gmail.com>
2022-01-28 20:47:56 -0300

    add put support to http_client

--- lib/notifier_web/http/http_client.ex
+++ lib/notifier_web/http/http_client.ex
@@ -2,4 +2,8 @@ defmodule NotifierWeb.HTTPClient do
   def post(url, body, headers \\ [{"Content-Type", "application/json"}]) do
     HTTPoison.post url, Jason.encode!(body), headers
   end
+
+  def put(url, body \\ "", headers \\ [{"Content-Type", "application/json"}]) do
+    HTTPoison.put url, Jason.encode!(body), headers
+  end
 end
